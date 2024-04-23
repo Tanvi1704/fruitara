6a3276be0722fc0041e9036fb8b452e2b2c006db
Tanvi Agrawal <110903222+Tanvi1704@users.noreply.github.com>
2024-04-23 23:30:57 +0530

    Update Addfood.jsx

--- client/src/pages/admin/Addfood.jsx
+++ client/src/pages/admin/Addfood.jsx
@@ -16,7 +16,7 @@ function Addfood() {
     setUploading(true);
     try {
       const { data } = await axios.post(
-        "http://fruitara.vercel.app/api/v1/all/upload-image/",
+        "https://fruitara.vercel.app/api/v1/all/upload-image/",
         formData
       );
       setUploading(false);
@@ -45,7 +45,7 @@ function Addfood() {
     // const foodData = { name, price, foodImage, catagory, weight, location, description};
 
     const res = await axios.post(
-      "http://fruitara.vercel.app/api/v1/food/addfood",
+      "https://fruitara.vercel.app/api/v1/food/addfood",
       { name, price, foodImage, catagory, weight, location, description },
       {
         headers: {
