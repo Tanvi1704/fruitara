f8d66fd568d0d3c9bef51a437851f88bec444dcd
Tanvi Agrawal <110903222+Tanvi1704@users.noreply.github.com>
2024-04-24 08:51:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ app.use("/api/v1/order", orderRoute);
 // Add middleware to include CORS headers
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://fruitara-frontend.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT");
+  res.setHeader("Access-Control-Allow-Methods", "GET", "POST", "PUT");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
